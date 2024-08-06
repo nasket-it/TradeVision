fe132ad8da7430c109405657b35130862f14b4dd
Alexei Trader <hetnask@gmail.com>
2024-08-06 21:29:07 +0300

    7

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ from dach_chart_tv import app
 
 
 if __name__ == '__main__':
-    port = 80
+    port = 5000
     host = '185.225.34.60'
-    app.run_server(debug=False, host=host, port=port)
+    app.run_server(debug=True)
