d31507446d3ce6d68b0cc0ce9d50ba9b4616f316
Alexei Trader <hetnask@gmail.com>
2024-08-06 21:42:56 +0300

    Мелкие правки

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ from dach_chart_tv import app
 if __name__ == '__main__':
     port = 5000
     host = '185.225.34.60'
-    app.run_server(debug=True)
+    app.run_server(debug=True, port=port, host=host)
