0b57f87e0dac598d061d566a7e57409d04a18ec8
Alexei Trader <hetnask@gmail.com>
2024-08-27 17:04:59 +0300

    Правка под размер маленьких экран4

--- elements_html_dash.py
+++ elements_html_dash.py
@@ -11,7 +11,7 @@ input_tiker = dbc.Input(id="input-tiker", type="str", placeholder="SBER",
                         style={"font-size": "1.6rem", 'margin-left' : 5, "width": "100%"})
 label_output_tiker = dbc.Label(id='label_output_tiker')
 button_enter = dbc.Button(id="button-enter",children="Enter",n_clicks=0,
-                            style={"font-size": "1.6rem", "width": "20%"}, color="primary", className="me-md-2")
+                            style={"font-size": "1.6rem", "width": "40%"}, color="primary", className="me-md-2")
 
 
 button_1m = dbc.Button("1m", id='time-frame1m', color="info", className="me-1", style={"font-size": "1.6rem", "width": "auto"})
